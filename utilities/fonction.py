afd051ec0a81b0d2793685f4b5478bd7b2caf374
“tellebma” <pdmtc.bellet@gmail.com>
2021-12-10 10:21:01 +0100

    create utilities folder

---
+++ utilities/fonction.py
@@ -0,0 +1,28 @@
+import re
+
+"""
+Regexs verification des données
+"""
+def isIntensite(element):
+    regex = "^[1-9][0-9]?$|^100$"
+    if re.match(regex,element):
+        return True
+    return False
+
+def isLatitude(element):
+    regex = "^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$"
+    if re.match(regex,element):
+        return True
+    return False
+
+def isLongitude(element):
+    regex = "^[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$"
+    if re.match(regex,element):
+        return True
+    return False
+
+def isCapteur(element):
+    regex = "^\d$"
+    if re.match(regex,element):
+        return True
+    return False
